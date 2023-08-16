e5588c93d3d998ad61c915cf072fb1b9b17bbd3c
Mauricio Ortega <mauricio.ortega.solis@gmail.com>
2023-08-15 20:20:44 -0400

    Estructura HTML

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const listaTareas=[
+    {   id: 1,
+        tarea:'Apagar despertador',
+        estado:false
+    },
+    {   id: 2,
+        tarea:'Levantarse',
+        estado:false
+    },
+    {   id: 3,
+        tarea:'Ducharse',
+        estado:false
+    }
+]
+
